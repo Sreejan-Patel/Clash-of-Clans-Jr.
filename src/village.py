9f84d23fc003ddb05fea6ed302b0778e809a8c31
Sreejan-Patel <sreejanp2012@gmail.com>
2022-03-09 02:59:48 +0530

    Adding Rage Functionality to Troops

--- src/village.py
+++ src/village.py
@@ -221,10 +221,7 @@ class Village():
             if self.king.status == 1:
                 self.king.king_attack_damage = self.king.king_attack_damage * 2
                 self.king.king_movement_speed = self.king.king_movement_speed * 2
-            for counter in range(10):
-                if self.troops.status[counter] == 1 :
-                    self.troops.damage = self.troops.damage * 2
-                    self.troops.movement_speed = self.troops.movement_speed * 2
+            self.troops.damage = self.troops.damage * 2
             self.rage.status_rage = 2
         elif self.rage.status_rage == 2:
             if self.rage_time >= 5:
@@ -232,10 +229,7 @@ class Village():
                 if self.king.status == 1 or self.king.king_movement_speed == 2:
                     self.king.king_attack_damage = self.king.king_attack_damage // 2
                     self.king.king_movement_speed = self.king.king_movement_speed // 2
-            for counter in range(10):
-                if self.troops.status[counter] == 1 or self.troops.movement_speed == 2:
-                    self.troops.damage = self.troops.damage // 2
-                    self.troops.movement_speed = self.troops.movement_speed // 2
+                self.troops.damage = self.troops.damage // 2
 
 
 
